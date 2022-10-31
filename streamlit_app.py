85168233e70928aba2ad073ce3b465fa0780f7a7
joeorris <94115382+joeorris@users.noreply.github.com>
2022-10-31 11:00:01 +0000

    moved stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,6 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-streamlit.stop()
-
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -35,6 +33,7 @@ try:
 except URLError as e:
   streamlit.error()
   
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
